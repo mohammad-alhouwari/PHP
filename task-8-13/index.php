3f3a29a4269e7c4dddeb0e0dd431fcd82e28b024
mohammad-alhouwari <mohammad.alouwari.1996@gmail.com>
2023-08-13 18:52:13 +0300

    ppl

--- task-8-13/index.php
+++ task-8-13/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+    <!-- database=crud  table=ppl -->
   <div class="container">
     <header class="d-flex justify-content-between my-4">
         <h1>Users List</h1>
